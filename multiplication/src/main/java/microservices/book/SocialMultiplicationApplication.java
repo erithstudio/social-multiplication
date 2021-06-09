7ec81630e01332d2cd6b37ea00cf9cf556233e34
denis.prasetio <denis.prasetio@primecash.co.id>
2021-06-10 02:30:25 +0700

    fix bugs

--- multiplication/src/main/java/microservices/book/SocialMultiplicationApplication.java
+++ multiplication/src/main/java/microservices/book/SocialMultiplicationApplication.java
@@ -1,11 +1,11 @@
-package microservices.book.multiplication;
+package microservices.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("microservices.book.gamification.repository")
+@EnableJpaRepositories("microservices.book.multiplication.repository")
 public class SocialMultiplicationApplication {
 
 	public static void main(String[] args) {
