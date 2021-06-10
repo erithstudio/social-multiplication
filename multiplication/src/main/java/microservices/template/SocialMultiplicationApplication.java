7f6794077f8c91fb0ba2727c2d348cb3f95541e1
denis.prasetio <denis.prasetio@primecash.co.id>
2021-06-11 04:33:44 +0700

    fix service registry

--- multiplication/src/main/java/microservices/template/SocialMultiplicationApplication.java
+++ multiplication/src/main/java/microservices/template/SocialMultiplicationApplication.java
@@ -3,7 +3,9 @@ package microservices.template;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableJpaRepositories("microservices.template.multiplication.repository")
 public class SocialMultiplicationApplication {
--- service-registry/src/main/java/microservices/template/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/microservices/template/serviceregistry/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package microservices.template;
+package microservices.template.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
