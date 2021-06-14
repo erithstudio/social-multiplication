139655aaa46fceb4318e4f758fea13779bfb9bce
denis.prasetio <denis.prasetio@primecash.co.id>
2021-06-15 03:17:19 +0700

    add admin server

--- service-registry/src/main/java/microservices/template/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/microservices/template/serviceregistry/ServiceRegistryApplication.java
@@ -1,5 +1,6 @@
 package microservices.template.serviceregistry;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAdminServer
 @EnableConfigServer
 public class ServiceRegistryApplication {
 
--- service-registry/src/main/java/microservices/template/serviceregistry/configuration/WebSecurityConfig.java
+++ service-registry/src/main/java/microservices/template/serviceregistry/configuration/WebSecurityConfig.java
@@ -1,15 +1,57 @@
 package microservices.template.serviceregistry.configuration;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
+    /*@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().and().authorizeRequests().antMatchers("/", "/login", "/eureka/**").permitAll();
         http.csrf().ignoringAntMatchers("/", "/login", "/eureka/**");
         super.configure(http);
+    }*/
+
+    private final String adminContextPath;
+
+    public WebSecurityConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(adminContextPath + "/");
+
+//        http.httpBasic().and().authorizeRequests().antMatchers("/", "/login", "/eureka/**").permitAll();
+//        http.csrf().ignoringAntMatchers("/", "/login", "/eureka/**");
+
+        http.authorizeRequests()
+                .antMatchers(adminContextPath + "/").permitAll()
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .antMatchers(adminContextPath + "/eureka/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                .logout().logoutUrl(adminContextPath + "/logout").and()
+                .httpBasic().and()
+                .csrf().ignoringAntMatchers(adminContextPath + "/", adminContextPath + "/login", adminContextPath + "/eureka/**")
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringRequestMatchers(
+                        new AntPathRequestMatcher(adminContextPath + "/instances", HttpMethod.POST.toString()),
+                        new AntPathRequestMatcher(adminContextPath + "/instances/*", HttpMethod.DELETE.toString()),
+                        new AntPathRequestMatcher(adminContextPath + "/actuator/**")
+                );
+
+        // super.configure(http);
     }
 }
